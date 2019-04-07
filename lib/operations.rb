baf4ccaf304e56d26392b8972d485dcb26e83cb6
Andrew Allen <asallen67@gmail.com>
2019-04-07 16:11:16 -0500

    1

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,8 @@
-def unsafe?(speed)
-
+def unsafe(speed)
+	return false if speed>=40 and speed<=60
+	return true
 end
 
-
-
-def not_safe?(speed)
-	
+def not_safe(speed)
+	speed>=40 && speed<=60 ? false : true
 end
-	
-
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
-# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require_relative '../lib/operations.rb'
-
 RSpec.configure do |config|
-  # config here
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'default'
 end
